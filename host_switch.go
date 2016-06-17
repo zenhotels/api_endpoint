e6cf21ebc07685f11ee7a92ab2bdbbaa3704fa61
Ilya Biin <i.biin@ostrovok.ru>
2016-06-17 11:34:52 +0300

    ++ file

---
+++ host_switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+type HostSwitch map[string]http.Handler
+
+// Implement the ServerHTTP method on our new type
+func (hs HostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Check if a http.Handler is registered for the given host.
+	// If yes, use it to handle the request.
+	if handler := hs[r.Host]; handler != nil {
+		handler.ServeHTTP(w, r)
+	} else {
+		// Handle host names for wich no handler is registered
+		http.Error(w, "Forbidden", 403) // Or Redirect?
+	}
+}
